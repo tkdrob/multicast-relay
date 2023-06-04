123e9cedca4699fa3730e584bdda080c04933acb
Robert Hillis <tkdrob4390@yahoo.com>
2023-06-04 11:55:56 -0400

    test

--- multicast-relay.py
+++ multicast-relay.py
@@ -676,7 +676,7 @@ class PacketRelay():
                             receivingInterface = tx['interface']
                             broadcastPacket = (origDstAddr == broadcast)
 
-                
+                count = len(self.transmitters) / 4
                 for tx in self.transmitters:
                     # Re-transmit on all other interfaces than on the interface that we received this packet from...
                     if receivingInterface == tx['interface']:
@@ -699,18 +699,19 @@ class PacketRelay():
                     #if dstPort == 1900:
                     #    print(tx)
                     if dstPort == 987:
-                        print(len(self.transmitters))
-                        if relayAddr == "10.1.4.255":
-                            print("ing")
-                            relayAddr = "255.255.255.255"
-                            broadcast = "255.255.255.255"
-                            print(tx)
-                            print(dstAddr)
-                            print(origDstAddr)
-                            #if tx['relay']['port'] == 987:
-                            #    print("bro")
-                            #    print(tx)
-                            #    broadcast = "255.255.255.255"
+                        if count == len(self.transmitters) / 4:
+                            if relayAddr == "10.1.4.255":
+                                print("ing")
+                                relayAddr = "255.255.255.255"
+                                broadcast = "255.255.255.255"
+                                print(tx)
+                                print(dstAddr)
+                                print(origDstAddr)
+                                count =- 1
+                                #if tx['relay']['port'] == 987:
+                                #    print("bro")
+                                #    print(tx)
+                                #    broadcast = "255.255.255.255"
                         else:
                             continue
 
